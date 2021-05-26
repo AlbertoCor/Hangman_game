e5bd280087332311824a403fc4352861d49c1308
AlbertoCor <ac.insite15@live.com>
2021-05-26 00:04:06 -0500

    Modifying base code and some considerations to apply to our code from others

--- Hangman_game.py
+++ Hangman_game.py
@@ -1,6 +1,6 @@
 import os
-# import pygame
 import random
+# import pygame
 
 # to be added to another file
 #------------------------------------------------------
@@ -15,6 +15,19 @@ def two_players():
     pass
 
 #------------------------------------------------------
+#Check this open file
+#(
+    #     words = []
+    # with open('./files/data.txt', 'r', encoding='utf-8') as file: # Here we get the words from the file and make a list
+    #     for line in file:
+    #         words.append(line.strip().upper())
+
+    # the_word = random.choice(words).upper() #Here we make the '_' list according to the random selected word
+    # the_secret_word = ['_' for letter in the_word]
+    # keep_trying = True
+# )
+
+
 
 def read():
     words = []
@@ -24,44 +37,65 @@ def read():
             # print(line)
     word = list(words)
     randomword = random.choice(word)
-    print(randomword)
-    lenght = list(enumerate(randomword.rstrip(),0))
-    # print(lenght)
-    numb_underline = len(randomword.rstrip())
+    print(f"\nEsta es la palabra secreta: {randomword}")
+    return randomword
+
+def hide_words(word):
+    the_secret_word = ['_' for letter in word]
+    numb_underline = len(word.rstrip())
     # print(numb_underline)
-    under_line = " _"
+    under_line = "_"
     print(under_line * numb_underline)
-    return lenght
 
-    # for hides in lenght:
-    #     hide_word.append("_")
-    # print(str(hide_word)[1:-1])           #use for quit brackets on list
-    
-    # word = list(enumerate(words))
-    # print(word)
-    # print(line)
+def compare_words(randomword, lines):
+    # lenght = list(enumerate(randomword.rstrip(),0))
+    # print(lenght)
+    # if guess_word in str(lenght):
+        # print("ther its a letter")        
+    rand_word = list(randomword)
+    hiden_lines = list(lines)
+    keep_try = True
+    while keep_try:
+        # assert len(user) == 1, "presiona mas de una letra"
+        os.system("cls")        #clear windows every try
+        for i in range(0, len(rand_word)):
+            if rand_word[i] == keep_try:
+                hiden_lines[i] = rand_word[i]
+        print(hiden_lines.upper())
+        print("\n")
+    print("you win")
 
+    #   for i in range(len(the_word)): #This is the check for every input
+    #         for l in range(len(the_word)):
+    #             the_letter = normalize(the_word[l])
+    #             if your_letter == the_letter:
+    #                 if your_letter == the_secret_word[l]:
+    #                     you_got_it = True
+    #                     continue
+    #                 else:
+    #                     the_secret_word[l] = the_word[l]
+    #                     points += 100
+    #                     you_got_it = True
+    #                     continue
+        
 
 def run():
-    read()
-    guess_word = input("Teclea una letra: ")
 
-    # while True:           #loop for  checking letters with secret word
-    #     pass        
+    rand_word = read()
+    # hide_words(rand_word)
+    # lines = hide_words()
+    lines = "blue"
+    # hide_letter = list(enumerate(rand_word))
+    # guess_word = input("Teclea una letra: ")
+    compare_words("blue", lines)
+       
 
-
-    # lenghtWord = len()
-    # while True:
-    #     print("guess the word >:V")
-    #     word
-    #     letter = input("type a letter: ")
-        
 
 if __name__ == "__main__":
     run()
-    # while True:
-    #     print("Guess the word >:V !!!")
-    #     read.lenght()
-    #     pass
+            
+
+#check ascii theme
+#https://dev.to/lakatos88/ascii-themes-node-js-cli-interface-to-generate-themed-ascii-art-4ck8
 
     
