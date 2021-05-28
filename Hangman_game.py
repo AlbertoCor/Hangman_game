83d7e406bbd6e72d3eb06af11031effe6e4213f7
Alberto Coronado <ac.insite15@live.com>
2021-05-28 15:56:18 -0500

    Update lines description and add a branch to testing code test_01 = compare if structure to underdash line appaer with user letter

--- Hangman_game.py
+++ Hangman_game.py
@@ -1,69 +1,66 @@
 import os
 import random
-import numpy
-# import pygame
+# import pygame             # pygame module version of hagman game
 
 # to be added to another file
 #------------------------------------------------------
-def display():
+def display():              # Screen dimensions usable for pygame module
     pass
 
-def menu():
+def menu():                 # Screen with menu of configurations  selected by the player
     pass
 
-def two_players():
+def two_players():          # 2 players mode 
     pass
 
 #------------------------------------------------------
 
-def read():
-    words = []
-    with open("Python_Intermedio\Hangman_Game\Data\data.txt", "r", encoding="utf-8") as f:
-        for line in f:
-            words.append(line.strip())
-    randomword = random.choice(words)
-    print(f"\nEsta es la palabra secreta: {randomword}")
-    return randomword
+def read():                                                                                     # In this function we read file.txt and return a random word from the file
+    words = []                                                                                  # An empty list will store the words from the file.txt
+    with open("Python_Intermedio\Hangman_Game\Data\data.txt", "r", encoding="utf-8") as f:      # function used to open a file from path writed as reading, encode as utf-8, renamed as f
+        for line in f:                                                                          # loop for format the data
+            words.append(line.strip())                                                          # List storage of words from file.txt and rip any blank space
+    randomword = random.choice(words)                                                           # A random word selected from formated file.txt 
+    print(f"\nEsta es la palabra secreta: {randomword}")                                        # A print statement for chechk secret word
+    return randomword                                                                           # Return the value it keep a random word selected from file .txt
 
-def normalize(s): # It removes the accents of a string
-    replacements = (
+def normalize(s):                                                                               # It removes the accents of a string
+    replacements = (                                                                            # A tuple contain special caracters will be simplified to verificate with word
         ("á", "a"),
         ("é", "e"),
         ("í", "i"),
         ("ó", "o"),
         ("ú", "u"),
     )
-    for a, b in replacements:
-        s = s.replace(a, b).replace(a.upper(), b.upper())
-    return s
+    for a, b in replacements:                                                                   # Loop for check all special caracters in tuple
+        s = s.replace(a, b).replace(a.upper(), b.upper())                                       # the variable s store for replaced special character to a letter
+    return s                                                                                    # Value character without aphostrophe returned as s variable
 
-def hide_words(word):
-    hide = ['_' for letter in word]     # With this loop generate number of underdash of the secret word
-    hide_word = "".join(hide)           # Here join underdash of a hidden secret word
-    print(hide_word)                    # Test output underdash of secret word
-    return hide
+def hide_words(word):                                                                           #
+    hide = ['_' for letter in word]                                                             # With this loop generate number of underdash of the secret word
+    hide_word = "".join(hide)                                                                   # Here join underdash of a hidden secret word
+    print(hide_word)                                                                            # Test output underdash of secret word
+    return hide                                                                                 # Return underdash number of letters in the secret word
 
-def compare_words(randomword, guess_letter, hide_word):      
+def compare_words(randomword, guess_letter, hide_word):                                         #
     # assert len(user) == 1, "presiona mas de una letra"
-    you_got_it = False
-    for l in range(len(randomword)):        #This is the check for every input
-        # print(l)
+    you_got_it = False                                                                          # Parameter will be false to 
+    for l in range(len(randomword)):                                                            # This is the check for every input
         # the_letter = normalize(randomword[l])
-        rand_word = randomword[l]
-        # print(rand_word)
-        if guess_letter == rand_word:
-            if guess_letter == hide_word[l]:
-                print("you dont guess one >:V")
+        rand_word = randomword[l]                                                               # Random word from read function, spell every letter as loop
+        if guess_letter == rand_word:                                                           # Condition if letter input from user and letter from random word to compare
+            if guess_letter == hide_word[l]:                                                    # Condition if letter input from user and 
                 you_got_it = True
-                # continue
+                continue
             else:
                 hide_word[l] = randomword[l]
-                print("you have guess one :V")
                 # points += 100
                 you_got_it = True
-                # continue
+                continue
     if you_got_it == False: #This is the life check
         input('You lose one life, PRESS A KEY TO CONTINUE')
+
+def life_points():
     # lifes -= 1
     # if lifes == -1:
     #     print(f'The word was {the_word}')
@@ -75,9 +72,6 @@ def compare_words(randomword, guess_letter, hide_word):
     #         continue
     #     else:
             # break
-
-
-def life_points():
     pass
 
 def run():
@@ -89,6 +83,7 @@ def run():
         printable_secret_word = ''.join(hide_word) #It's the random selected word but like this ______ and it's being updated
         if printable_secret_word == rand_word: #This is the win check
             print("you win")
+            break
         print(printable_secret_word)
         print(f"Esta es la palabra secret: {rand_word}")
         guess_letter = input("\nTeclea una letra: ")
